9676055358a7b4ba110a7b2ce0bb285aaad85fbe
Sam Shadwell <sshadwell978@gmail.com>
2024-05-01 11:41:53 -0400

    Forked and cloned exercise 2, added lines to complete exercise

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Please enter your favorite school subject:");
+            string userFavSubject = Console.ReadLine();
+
+            switch(userFavSubject.ToLower())
+            {
+                case "english":
+                    Console.WriteLine("English? Guess you like to read, nerd.");
+                    break;
+                case "history":
+                    Console.WriteLine("History? What a snooze-fest.");
+                    break;
+                case "math":
+                    Console.WriteLine("Math? Gross, no one likes math.");
+                    break;
+                case "science":
+                    Console.WriteLine("Science? Wow, you're definitely a nerd.");
+                    break;
+                case "gym":
+                    Console.WriteLine("Gym? Should've known you were a gross jock.");
+                    break;
+                default:
+                    Console.WriteLine("Try again and actually enter a school subject this time.");
+                    break;
+            }
         }
     }
 }
